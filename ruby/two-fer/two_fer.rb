f8e265e9514bd5beb80d2ba5495057032e4845d1
jairorjuela <genius85862@gmail.com>
2019-04-21 18:23:16 -0500

    Exercise 2 Two Fer

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,9 +1,5 @@
 class TwoFer
-  def self.two_fer(name = nil)
-    options = {
-      true => -> { "One for you, one for me." },
-      false => -> { "One for #{name}, one for me." }
-    }
-    options[name.eql?(nil)].()
+  def self.two_fer(name = "you")
+    "One for #{name}, one for me."
   end
 end
