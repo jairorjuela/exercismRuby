38dd072f91be11042bcc276b68bd92bebe8fe43f
jairorjuela <genius85862@gmail.com>
2019-05-17 23:00:02 -0500

    Pangram solution

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,20 +1,5 @@
 class Pangram
   def self.pangram?(phrase)
-    if(phrase.length == 0)
-      false
-    else
-      order_phrase = phrase.chars.sort(&:casecmp).join
-      p ('a'..'z').all? { |word| phrase.downcase.include? (word) }
-      find_numbers = order_phrase.gsub(/(\d+)/, 'Z')
-      if(find_numbers.include?('Z'))
-        false
-      else
-        true
-      end
-    end
+    ('a'..'z').all? { |word| phrase.downcase.include? (word) }
   end
 end
-
-sentence = 'a quick movement of the enemy will jeopardize five gunboats'
-result = Pangram.pangram?(sentence)
-p result
