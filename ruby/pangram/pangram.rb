227918a5ebc9626a4262687ef640618f893a057f
jairorjuela <genius85862@gmail.com>
2019-05-24 23:12:15 -0500

    Matrix, create file, delete garbage code

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -3,34 +3,3 @@ class Pangram
     ('a'..'z').all? { |word| phrase.downcase.include? (word) }
   end
 end
-
-
-import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-    constructor(props) {
-      super(props);
-
-  this.state = {
-    value: 0
- }
-}
-
-  render() {
-    return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc" onClick={this.state.value} onChange={this.updateValue.bind(this)} >Incrementa</button>
-      </div>
-    );
-  }
-
-  updateValue(event)  {
-    this.setState({
-        value: event.target.value+1
-    });
-  }
-}
-
-export default App;
