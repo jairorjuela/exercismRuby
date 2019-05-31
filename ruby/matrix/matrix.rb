fe8ac0e088ee0dbc07598a8eba27cf6e26f77116
jairorjuela <genius85862@gmail.com>
2019-05-30 22:33:14 -0500

    Rows method

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -5,10 +5,12 @@ class Matrix
 
   def rows
     i = 0
-    count = @matrix.split("\n").length
-    while i < count do
-      count[i].split
+    count = @matrix.split("\n")
+    rows = []
+    while i < count.length do
+      rows << count[i].split.map { |s| s.to_i }
       i += 1
     end
+    rows
   end
 end
