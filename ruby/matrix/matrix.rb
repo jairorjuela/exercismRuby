a822254e19b2382f7c21dfb1a874dea4dcf08657
jairorjuela <genius85862@gmail.com>
2019-05-30 23:04:15 -0500

    Columns try  method

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -13,4 +13,19 @@ class Matrix
     end
     rows
   end
+
+  def columns
+    i = 0
+    count = @matrix.split("\n")
+    rows = []
+    while i < count.length do
+      rows << count[i].split.map { |s| s.to_i }
+      i += 1
+    end
+    rows
+  end
 end
+
+
+matrix = Matrix.new("89 1903 3\n18 3 1\n9 4 800")
+p matrix.columns
