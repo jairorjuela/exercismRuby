4053ddda966e99a8c047c819bbb4573e1f18eae0
jairorjuela <genius85862@gmail.com>
2019-05-31 22:14:00 -0500

    Solution

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -15,17 +15,6 @@ class Matrix
   end
 
   def columns
-    i = 0
-    count = @matrix.split("\n")
-    rows = []
-    while i < count.length do
-      rows << count[i].split.map { |s| s.to_i }
-      i += 1
-    end
-    rows
+    rows.transpose
   end
 end
-
-
-matrix = Matrix.new("89 1903 3\n18 3 1\n9 4 800")
-p matrix.columns
