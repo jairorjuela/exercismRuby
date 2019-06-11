436b66715927df261868d130c6e9da0e22a06ac2
jairorjuela <genius85862@gmail.com>
2019-06-10 22:33:34 -0500

    Result exercism other solution

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -4,24 +4,12 @@ class Matrix
   end
 
   def rows
-    rows = []
-    test =  @matrix.each_line {|s| rows << s}
-    #test.map { |s| s.to_i }
-    rows.map { |s| p "hola #{s}" }
-    #@matrix.each_line {|s| rows << s}
-    # while i < count.length do
-    #   rows << count[i].split.map { |s| s.to_i }
-    #   i += 1
-    # end
-    #rows
+    @matrix.each_line.map do |rows|
+      rows.split.map(&:to_i)
+    end
   end
 
   def columns
     rows.transpose
   end
 end
-
-
-matris = Matrix.new("9 8 7\n19 18 17")
-
-p matris.rows
