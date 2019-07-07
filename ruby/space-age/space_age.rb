534426867221928f6c83782994b70a117234db40
jairorjuela <genius85862@gmail.com>
2019-07-07 16:52:50 -0500

    Test method on mercury

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,10 +1,33 @@
 class SpaceAge
   def initialize(age)
     @age = age
+    @year_earth = 31557600
   end
 
   def on_earth
-    earth = @age.to_f / 31557600
+    earth = @age.to_f / @year_earth
     earth.round(2)
+  end
+
+  def on_mercury
+    p @age
+    age_mercury = @age / 365.25
+    p age_mercury
+    #mercury = age_mercury * @year_earth
+    #mercury.round(2)
+  end
+
+end
+def truncate(string, max)
+  if max.zero?
+    ""
+  else
+    if string.length >= max
+      "#{string[0..max-1]}"
+    else
+      space = " " * max
+      space1 = space.length - string.length
+      string + "#{space[0..space1 - 1]}"
+    end
   end
 end
